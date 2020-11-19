4213e706abaee556b2daaf5e34c50fb5364ed9e4
windydesty <destyw540@gmail.com>
2020-11-19 16:00:02 +0700

    tucil

---
+++ ci4/app/Controllers/Auth.php
@@ -0,0 +1,27 @@
+<?php namespace App\Controllers;
+
+class Auth extends BaseController
+{
+
+    public function index() 
+{ return view('index');
+} 
+
+public function about() 
+{ return view('about');
+}
+
+public function login() 
+{ return view('login');
+}
+
+public function register() 
+{ return view('register');
+}
+
+public function admin() 
+{ return view('admin');
+} 
+
+
+}
---
+++ ci4/app/Views/index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel ="stylesheet" href="/css/style.css">
+    
+
+    <title>Web Hobi Makan</title>
+  </head>
+  <body>
+ <!-- Awal navigasi bar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+ <div class="container">
+  <a class="navbar-brand font-weight-bold" href="<?= base_url('auth/index'); ?>">Web Hobi Makan</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?= base_url('auth/admin'); ?>">Admin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('auth/index'); ?>">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('auth/login'); ?>">Log Out</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
+
+<!-- Akhir navigation bar -->
+
+<!-- Jumbotron -->
+
+  <div class="container text-center text-black ">
+  <img src="/img/seblak.jpg" width="200px"class="rounded">
+   <h1 class="display-4font-weight-bold">Seblak</h1>
+    <p class="lead">dengan berbagai level kepedasan</p>
+  </div>
+
+<!-- Akhir Jumbotron -->
+
+<!-- discography -->
+
+
+  <div class="container text-center text-black ">
+  <img src="/img/bakso.jpg" width="200px"class="rounded">
+   <h1 class="display-4font-weight-bold">Bakso</h1>
+    <p class="lead">dengan berbagai macam jenis isian bakso</p>
+  </div>
+
+
+<
+  <div class="container text-center text-black ">
+  <img src="/img/taichan.jpg" width="200px"class="rounded">
+   <h1 class="display-4font-weight-bold">Taichan</h1>
+    <p class="lead">sate ayam dengan sambal pedas</p>
+  </div>
+
+
+  <div class="container text-center text-black ">
+  <img src="/img/mie.jpg" width="200px"class="rounded">
+   <h1 class="display-4font-weight-bold">Mie Ayam</h1>
+    <p class="lead">Mie ayam dengan kuah kaldu</p>
+  </div>
+
+
+  <div class="container text-center text-black ">
+  <img src="/img/ceker.jpg" width="200px"class="rounded">
+   <h1 class="display-4font-weight-bold">Ceker Mercon</h1>
+    <p class="lead">ceker ayam dengan sambal mercon</p>
+  </div>
+
+
+<!-- discography -->
+
+ 
+   
+
+   
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+  
+  </body>
+</html>
